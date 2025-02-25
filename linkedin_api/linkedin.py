54601c55b19d54e03ebad96055a64386ac82a5fa
pritesh066 <89059587+pritesh066@users.noreply.github.com>
2025-02-25 14:58:30 +0530

    fix:try except add

--- linkedin_api/linkedin.py
+++ linkedin_api/linkedin.py
@@ -747,117 +747,18 @@ class Linkedin(object):
         """
         # NOTE this still works for now, but will probably eventually have to be converted to
         # https://www.linkedin.com/voyager/api/identity/profiles/ACoAAAKT9JQBsH7LwKaE9Myay9WcX8OVGuDq9Uw
-        print(public_id, urn_id,".........................")
-        res = self._fetch(f"/identity/profiles/{public_id or urn_id}/profileView")
-        print("::::::::::::::::::",res)
-        # if data and "status" in data and data["status"] != 200:
-        if res.status_code != 200:
-            self.logger.info("request failed: {}".format(res.status_code))
-            # self.logger.info("request failed: {}".format(data["message"]))
+        try:
+            res = self._fetch(f"/identity/profiles/{public_id or urn_id}/profileView")
+            # if data and "status" in data and data["status"] != 200:
+            if res.status_code != 200:
+                self.logger.info("request failed: {}".format(res.status_code))
+                # self.logger.info("request failed: {}".format(data["message"]))
+                return {}
+            data = res.json()
+            return data
+        except Exception as e:
             return {}
-        data = res.json()
-        return data
-        # massage [profile] data
-        profile = data["profile"]
-        if "miniProfile" in profile:
-            if "picture" in profile["miniProfile"]:
-                profile["displayPictureUrl"] = profile["miniProfile"]["picture"][
-                    "com.linkedin.common.VectorImage"
-                ]["rootUrl"]
-
-                images_data = profile["miniProfile"]["picture"][
-                    "com.linkedin.common.VectorImage"
-                ]["artifacts"]
-                for img in images_data:
-                    w, h, url_segment = itemgetter(
-                        "width", "height", "fileIdentifyingUrlPathSegment"
-                    )(img)
-                    profile[f"img_{w}_{h}"] = url_segment
-
-            profile["profile_id"] = get_id_from_urn(profile["miniProfile"]["entityUrn"])
-            profile["profile_urn"] = profile["miniProfile"]["entityUrn"]
-            profile["member_urn"] = profile["miniProfile"]["objectUrn"]
-            profile["public_id"] = profile["miniProfile"]["publicIdentifier"]
-
-            del profile["miniProfile"]
-
-        del profile["defaultLocale"]
-        del profile["supportedLocales"]
-        del profile["versionTag"]
-        del profile["showEducationOnProfileTopCard"]
-
-        # massage [experience] data
-        experience = data["positionView"]["elements"]
-        for item in experience:
-            if "company" in item and "miniCompany" in item["company"]:
-                if "logo" in item["company"]["miniCompany"]:
-                    logo = item["company"]["miniCompany"]["logo"].get(
-                        "com.linkedin.common.VectorImage"
-                    )
-                    if logo:
-                        item["companyLogoUrl"] = logo["rootUrl"]
-                del item["company"]["miniCompany"]
-
-        profile["experience"] = experience
-
-        # massage [education] data
-        education = data["educationView"]["elements"]
-        for item in education:
-            if "school" in item:
-                if "logo" in item["school"]:
-                    item["school"]["logoUrl"] = item["school"]["logo"][
-                        "com.linkedin.common.VectorImage"
-                    ]["rootUrl"]
-                    del item["school"]["logo"]
-
-        profile["education"] = education
-
-        # massage [languages] data
-        languages = data["languageView"]["elements"]
-        for item in languages:
-            del item["entityUrn"]
-        profile["languages"] = languages
-
-        # massage [publications] data
-        publications = data["publicationView"]["elements"]
-        for item in publications:
-            del item["entityUrn"]
-            for author in item.get("authors", []):
-                del author["entityUrn"]
-        profile["publications"] = publications
-
-        # massage [certifications] data
-        certifications = data["certificationView"]["elements"]
-        for item in certifications:
-            del item["entityUrn"]
-        profile["certifications"] = certifications
-
-        # massage [volunteer] data
-        volunteer = data["volunteerExperienceView"]["elements"]
-        for item in volunteer:
-            del item["entityUrn"]
-        profile["volunteer"] = volunteer
-
-        # massage [honors] data
-        honors = data["honorView"]["elements"]
-        for item in honors:
-            del item["entityUrn"]
-        profile["honors"] = honors
-
-        # massage [projects] data
-        projects = data["projectView"]["elements"]
-        for item in projects:
-            del item["entityUrn"]
-        profile["projects"] = projects
-        # massage [skills] data
-        skills = data["skillView"]["elements"]
-        for item in skills:
-            del item["entityUrn"]
-        profile["skills"] = skills
-
-        profile["urn_id"] = profile["entityUrn"].replace("urn:li:fs_profile:", "")
-
-        return profile
+            
 
     def get_profile_connections(self, urn_id: str, **kwargs) -> List:
         """Fetch connections for a given LinkedIn profile.
